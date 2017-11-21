dd84ed6e04b2a125ea63bdb1e771cdafc29d1e93
lucasdevico <lucas.devico@gmail.com>
2017-11-21 00:36:37 -0200

    Criação de log

--- server/config/express.js
+++ server/config/express.js
@@ -2,6 +2,8 @@ var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
 var app = express();
+var logger = require("../utils/logger");
+var morgan = require('morgan');
 
 app.use(express.static('../client'));
 app.use(bodyParser.json());
@@ -14,6 +16,10 @@ app.use(function(req, res, next) { //allow cross origin requests
 });
 
 app.set('secret', 'MaLiBu28032007FuTsAl');
+
+app.use(morgan("dev", {
+	stream: logger.stream
+}));
 
 consign({cwd: 'app'})
 	.include('models')
